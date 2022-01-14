c94b1ac5cd5a50a3f6255fed269919d28985c825
rhit-ramjia <ramjia@rose-hulman.edu>
2022-01-14 12:30:56 -0500

    Changed fragment layouts, widgets

--- RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/ui/MessageTutorFragment.kt
+++ RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/ui/MessageTutorFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.rosetutortracker.R
 import com.example.rosetutortracker.databinding.FragmentMessageTutorBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MessageTutorFragment: Fragment() {
     private lateinit var binding: FragmentMessageTutorBinding
@@ -16,6 +19,23 @@ class MessageTutorFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMessageTutorBinding.inflate(inflater, container, false)
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.sendButton.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Send message?")
+                .setMessage("Are you sure you want to send this message?")
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    //Have the message actually sent
+                    findNavController().popBackStack()
+                    findNavController().popBackStack()
+                }
+                .setNegativeButton(android.R.string.cancel,null)
+                .show()
+        }
 
         return binding.root
     }
