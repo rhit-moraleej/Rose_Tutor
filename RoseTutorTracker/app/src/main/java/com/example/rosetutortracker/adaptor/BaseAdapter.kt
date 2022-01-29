2226d420edab13ee48e6e4777d8a6ab52c1d86f2
Elvis Morales Campoverde <moraleej@rose-hulman.edu>
2022-01-28 20:35:35 -0500

    attempting to create an abstract adaptor class

--- RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/adaptor/BaseAdapter.kt
+++ RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/adaptor/BaseAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.rosetutortracker.adaptor
 
-class BaseAdapter(val ) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rosetutortracker.models.BaseViewModel
+import com.example.rosetutortracker.models.HomeViewModel
+
+abstract class BaseAdapter<T>(val fragment: Fragment,
+                           viewModel: BaseViewModel<T>,
+                           ): RecyclerView.Adapter<BaseAdapter<T>.ViewHolder>() {
+    val model = ViewModelProvider(fragment.requireActivity())[viewModel :: class.java]
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(model.getListAt(position))
+    }
+
+    override fun getItemCount() = model.size()
+
+    abstract inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        abstract fun bind(item: T)
+    }
 }
