5eb9a283f0f43608697a3ed0411713b570807190
rhit-ramjia <ramjia@rose-hulman.edu>
2022-02-10 20:09:59 -0500

    nav header shows name, email, profile pic

--- RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/ui/HomeFragment.kt
+++ RoseTutorTracker/app/src/main/java/com/example/rosetutortracker/ui/HomeFragment.kt
@@ -5,10 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.example.rosetutortracker.R
 import com.example.rosetutortracker.adaptor.FavTutorAdaptor
 import com.example.rosetutortracker.databinding.FragmentHomeBinding
+import com.example.rosetutortracker.models.StudentViewModel
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -28,6 +36,22 @@ class HomeFragment : Fragment() {
         binding.recyclerView.adapter = adaptor
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.setHasFixedSize(true)
+
+        var studentModel = ViewModelProvider(requireActivity())[StudentViewModel::class.java]
+        var navView: NavigationView = requireActivity().findViewById(R.id.nav_view)
+        val navHeaderView = navView.getHeaderView(0)
+        val userName: TextView = navHeaderView.findViewById(R.id.user_name)
+        val userEmail: TextView = navHeaderView.findViewById(R.id.user_email)
+        val userProfile: ImageView = navHeaderView.findViewById(R.id.user_profile)
+        Log.d("navView",userName.text.toString())
+        Log.d("navView",userEmail.text.toString())
+        userName.text = studentModel.student?.name ?: ""
+        userEmail.text = studentModel.student?.email ?: ""
+        userProfile.load(studentModel.student!!.storageUriString) {
+            crossfade(true)
+            transformations(CircleCropTransformation())
+        }
+
 
         Log.d("rr", Firebase.auth.currentUser!!.uid)
 
